2f4717a0e1c9544e6831b8d04fc6a8e5201cb13c
Oscar Ventura <oventura115@gmail.com>
2024-07-10 09:14:18 -0300

    Se realizo el ejercicio 1

---
+++ JS/ejercicio1.js
@@ -0,0 +1,8 @@
+/* 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.*/
+const edad = parseInt(prompt("Ingrese una edad"))
+if(edad >= 18 && edad<=75){
+    document.write(`Con la edad de ${edad} años ya puede conducir`)
+}else{
+    document.write(`Con la edad de ${edad} años no puede conducir`)
+}
+console.log(edad) 
